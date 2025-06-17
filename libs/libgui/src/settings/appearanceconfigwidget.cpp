153d271787f9673f0cbba4bc7db8f80533cfe9e0
Raphael Araújo e Silva <raphael@pgmodeler.io>
2025-06-17 08:34:41 -0300

    Minor adjustments in AppearanceConfigWidget

--- libs/libgui/src/settings/appearanceconfigwidget.cpp
+++ libs/libgui/src/settings/appearanceconfigwidget.cpp
@@ -58,7 +58,7 @@ std::map<QPalette::ColorRole, QStringList> AppearanceConfigWidget::light_ui_colo
 	{ QPalette::WindowText, {"#232627", "#232627", "#777878"} },
 	{ QPalette::Button, {"#eff0f1", "#eff0f1", "#eff0f1"} },
 	{ QPalette::Light, {"#ffffff", "#ffffff", "#ffffff"} },
-	{ QPalette::Midlight, {"#ffffff", "#ffffff", "#ffffff"} },
+	{ QPalette::Midlight, {"#c6c7c8", "#b0b1b2", "#b0b1b2"} },
 	{ QPalette::Mid, {"#9fa0a1", "#9fa0a1", "#9fa0a1"} },
 	{ QPalette::Dark, {"#777878", "#777878", "#777878"} },
 	{ QPalette::Text, {"#232627", "#232627", "#777878"} },
